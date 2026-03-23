8ad7c94283498d3d9c8f86b0faf86a655c1142e0
DASOM <ektha115@gmail.com>
2026-03-24 00:27:08 +0900

    fix SQL 스크립트 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE product (
+CREATE TABLE IF NOT EXISTS product (
     id bigint not null auto_increment primary key,
     name varchar(255) not null,
     price bigint not null default 0
